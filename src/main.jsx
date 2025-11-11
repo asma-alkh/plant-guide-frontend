5848f67f209a2f21ee6669a86412a0a7eee691a5
asma2003 <asma200m2200@gmail.com>
2025-11-11 23:10:23 +0300

    Final deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // <-- أضف هذا
-import './index.css';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>          {/* <-- لف App بالـ HashRouter */}
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>
